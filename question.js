e9d6da8650ba6823e793ec7716625d92014730b8
Amanda Heideman <heideman.amanda@gmail.com>
2020-10-03 11:29:23 +0200

    redo

--- question.js
+++ question.js
@@ -1,8 +1,7 @@
 class Question{
-    constructor(result){
-        this.q = result.question; //result[0].question är string
-        this.a = result.answers; //result[0].answers är object
-        /* console.log(this.q); //result är JSON / object
-        console.log(typeof result[0].answers); */
+    constructor(){
+        this.q;    // result.question; //result[0].question är string
+        this.a;    //result.answers; //result[0].answers är object
+        
     }
 }
