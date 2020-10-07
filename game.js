1ee1f08cd8cfb939187f9c9695aa417e4014fa73
Amanda Heideman <heideman.amanda@gmail.com>
2020-10-07 22:01:06 +0200

    moved fetch to Question-class

--- game.js
+++ game.js
@@ -3,17 +3,10 @@ class Game{
         this.rounds = number_of_rounds;
         this.player = player;
 
-        for( let i = 0; i<this.rounds; i++){
-            this.getQuestion(i);
-        }
+        let questions = new Question(this.rounds);
         
-    }
-    getQuestion(i){
-        fetch("https://quizapi.io/api/v1/questions?apiKey=lAbxxIyZH7KpJEH70P6eGGbueQmlZ4KHdQsk7BVV")
-        .then(response => response.json())
-        .then(result => {
-            let question = new Question(result, i)
-        }
-        )
-    }
+        
+        
+    } 
+    
 }
--- question.js
+++ question.js
@@ -1,11 +1,31 @@
 class Question{
-    constructor(result, i=0){
-        this.q = result[i].question;    // result.question; //result[0].question är string
-        this.a = result[i].answers;   //result.answers; //result[0].answers är object
-        console.log(this.q);
-        console.log(typeof this.q);
-        console.log(this.a);
-        console.log(typeof this.a);
+    constructor(i){
+        this.question = []; //används inte i nuläget
+        this.getQuestion(i);
+        //this.printQuestion();
+        
     }
-    
+    getQuestion(i){
+        fetch("https://quizapi.io/api/v1/questions?apiKey=lAbxxIyZH7KpJEH70P6eGGbueQmlZ4KHdQsk7BVV&limit="+i)
+        .then(response => response.json())
+        .then(result => { //result = vårt api-svar i objektformat
+            for(let j=0; j<i; j++){
+                this.question.push(result[j]);
+                this.printQuestion(result[j]);
+            }
+        })
+        
+    }
+    printQuestion(result){
+        let div = document.getElementById("div");
+        let question_field = document.createElement("p");
+        let answer_list = document.createElement("p");
+        
+
+        question_field.innerHTML = result.question;
+        answer_list.innerHTML = result.answers; //fixa
+
+        div.appendChild(question_field).after(answer_list);
+        
+    }
 }
