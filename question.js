1f2a54c4ea3edf34bb70239b4962bcf74a4da197
Amanda Heideman <heideman.amanda@gmail.com>
2020-10-08 08:51:13 +0200

    questions and answers get printed on screen

--- question.js
+++ question.js
@@ -1,6 +1,6 @@
 class Question{
     constructor(i){
-        this.question = []; //används inte i nuläget
+        //this.question = []; //används inte i nuläget
         this.getQuestion(i);
         //this.printQuestion();
         
@@ -10,20 +10,29 @@ class Question{
         .then(response => response.json())
         .then(result => { //result = vårt api-svar i objektformat
             for(let j=0; j<i; j++){
-                this.question.push(result[j]);
+                //this.question.push(result[j]);
                 this.printQuestion(result[j]);
             }
         })
         
     }
     printQuestion(result){
+        //skriver ut frågor och svarsalternativ på sidan
+
         let div = document.getElementById("div");
         let question_field = document.createElement("p");
-        let answer_list = document.createElement("p");
+        let answer_list = document.createElement("ol");
         
-
+        let answers_arr = Object.values(result.answers); 
         question_field.innerHTML = result.question;
-        answer_list.innerHTML = result.answers; //fixa
+
+        for (let element of answers_arr){   //lägger till alla svarsalternativ i en lista
+            if(element!=null){
+                let a = document.createElement("li");
+                a.innerHTML = element;
+                answer_list.appendChild(a);
+            }
+        }
 
         div.appendChild(question_field).after(answer_list);
         
