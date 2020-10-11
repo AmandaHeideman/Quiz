3bf3cc7dac92e6bbff386861b13e592d0e58501c
Amanda Heideman <heideman.amanda@gmail.com>
2020-10-12 00:34:30 +0200

    moved startscreen to startNewGame class, added option to play again

--- startNewGame.js
+++ startNewGame.js
@@ -1,48 +1,70 @@
 class startNewGame{
     constructor(){
-        //skapar startsidan där spelare får mata in namn och antal spel
+        var self = this;
+       //skapar startsidan där spelare får mata in namn och antal spel
         let div = document.getElementById("div"); 
 
         let name_q = document.createElement("p");
-        name_q.innerHTML = "Enter you name:";
+        name_q.innerHTML = "Enter your name:";
         let name_input = document.createElement("input");
 
         let storeName = localStorage.getItem("name_input")
         name_input.value = storeName;
+        this.nameInput = name_input.value;
 
         let game_q = document.createElement("p");
         game_q.innerHTML = "Enter number of questions:";
         let game_input = document.createElement("input");
+        game_input.value = 5;
+        this.gameInput = game_input.value;
 
         let btnDiv = document.getElementById("btnDiv");
-        let btn = document.createElement("button");
-        btn.innerHTML = "Next";
+        this.btn = document.createElement("button");
+        this.btn.innerHTML = "Next";
 
         //lägger skapade element i div:en så de syns på sidan
         div.appendChild(name_q).after(name_input); 
         div.appendChild(game_q).after(game_input);
-        btnDiv.appendChild(btn);
+        btnDiv.appendChild(this.btn);
 
-        let d = div.innerHTML;
-        let n = name_input.value;
-        let g = game_input.value;
-        btn.addEventListener("click", this.start(d, n, g));
+        this.game;
+        this.player;
+        this.i = 1;
+        this.btn.addEventListener("click", function(e){
+            self.start()
+        });
     }
-    start(div, name_input, game_input){
-        let game;
-        //rensar div:en så sidan blir tom, förutom knappen som har en egen div
-        div = "";
-
-        if (game==null){ //startar ett nytt spel ifall det inte redan finns ett
-
+    
+    start(){
+        
+        //startar ett nytt spel ifall det inte redan finns ett
+        if (this.game==null){ 
             //sparar spelarens namn i localStorage
-            localStorage.setItem("name_input", name_input);
-
-            let player = new Player(name_input);
-            game = new Game(game_input, player);
+            localStorage.setItem("name_input", this.nameInput);
+            
+            this.player = new Player(this.nameInput);
+            this.game = new Game(this.gameInput, this.player);
         }
-
-        game.getQuestion();
-        game.playerAnswers();
+        if(this.i<=this.gameInput){
+            this.game.playerAnswers();
+            this.game.getQuestion(this.i);
+            this.i++
+            
+            //rensar div:en så sidan blir tom inför nästa fråga, förutom knappen som har en egen div
+            div.innerHTML = "";
+        }
+        else if(this.i>this.gameInput){
+            div.innerHTML = "Game over"
+            let endscreen = document.createElement("p");
+            endscreen.innerHTML = this.player.name + " got " + this.player.points + " points"
+            div.appendChild(endscreen);
+            this.btn.innerHTML = "Play again?"
+            this.btn.addEventListener("click", function(e){
+                btnDiv.innerHTML = "";
+                div.innerHTML ="";
+                
+                let start = new startNewGame();
+            });
+        }
     }
 }
